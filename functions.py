f1c4d00a8e928986622be629470fec22fcc14250
peter <pjkrupa@proton.me>
2025-11-24 12:06:27 +0100

    adds RAG client and tool handler, refactors main logic to reflect the new class objects

--- functions.py
+++ functions.py
@@ -42,14 +42,7 @@ class EmbeddingsClient:
     @handle_api_errors(default=[])
     def embed(self, text: str) -> list[float]:
         """
-        Hits a custom API that uses a transformer to calculate embeddings.
-
-        Parameters:
-        text -> The text to be converted to embeddings, str.
-        config -> Configurations object
-
-        Returns:
-        The embedding, list[float]
+        Hits embeddings API to return vector embeddings 
         """
         logger = self.configs.logger
         start = time.time()
@@ -68,7 +61,6 @@ class EmbeddingsClient:
         logger.info("Successfully fetched embedding.")
         logger.info(f"RESPONSE TIME: {time.time() - start:.3f}s")
         logger.info(f"EMBED FETCH STATUS: {resp.status_code}")
-        logger.info("-" * 50)
 
         return embedding
 
@@ -79,7 +71,7 @@ class EmbeddingsClient:
         results: list[ChromaDbResult],
         ) -> dict:
         """
-        Runs reranking on the ChromaDB results and returns the IDs of the top_n query responses.
+        Hits an embedding API to run reranking on the ChromaDB results and return the IDs of the top_n query responses.
 
         Parameters:
         query_text -> the text of the original query, str
@@ -130,7 +122,7 @@ class LiteLlmClient:
         self.tools = tools
 
     # need to add error handling and retries for this.
-    def request(self, messages: list[Message]
+    def send_request(self, messages: list[Message]
         ) -> Message:
         params = Parameters(
                 model=self.configs.model, 
@@ -144,115 +136,130 @@ class LiteLlmClient:
         lite_msg = response.choices[0].message
         return Message.model_validate(lite_msg.model_dump())
 
-#FINISH THIS. 
+
+class ToolHandler:
+
+    def __init__(self, configs: Configurations, tools: list[Tool]):
+        self.configs = configs
+        self.tools = tools
+        self.tool_names = [tool.function.name for tool in tools]
+        self.logger = configs.logger
+        self.rag = RagClient(configs, tools)
+
+    def handle(self, message: Message):
+        for tool_call in message.tool_calls:
+
+            if tool_call.function.name not in self.tool_names:
+                self.logger.error(f"The model tried to call tool {tool_call.function.name}, which is not in the list of tool names: {self.tool_names} ")
+                return Message(role='tool', tool_call_id=tool_call.id, content='There is no tool with that name')
+            
+            # need an elif block for each name in tool_names
+            elif tool_call.function.name == "gdpr_query":
+                arguments = json.loads(tool_call.function.arguments)
+                self.logger.info(f"Call for tool {tool_call.function.name}: {arguments}")
+                return self.rag.generate( 
+                    query=arguments["query_text"], 
+                    tool_call_id=message.tool_call_id, 
+                    collection="gdpr")
+            
+            # logs an error if a tool exists but handling hasn't been added yet
+            else:
+                self.logger.error(f"Tool call for {tool_call.function.name} not handled. Have you added handling for it yet??")
+                return Message(role='tool', tool_call_id=tool_call.id, content='Tool not found.')
+
+
 class RagClient:
-    def __init__(self, configs: Configurations):
+
+    def __init__(self, configs: Configurations, tools: list[Tool]):
         self.configs = configs
         self.emb_client = EmbeddingsClient(configs=configs)
-# ------------------------------
 
-@handle_api_errors(default={})
-def query_collection(
-        query_embedding: list[float],
-        collection: str,
-        config: Configurations
+    def generate(self, query: str, collection: str, tool_call_id: str) -> Message:
+        vec_query = self.emb_client.embed(text=query)
+        chroma_docs = self._get_docs(query_embedding=vec_query, collection=collection)
+        reranked = self.emb_client.rerank(query_text=query, results=chroma_docs)
+        final_results = self._filter_results(results=chroma_docs, reranked=reranked["results"])
+        json_str = json.dumps([obj.model_dump() for obj in final_results])
+        return Message(role='tool', tool_call_id=tool_call_id, content=json_str)
+    
+    @handle_api_errors(default={})
+    def _get_docs(self,
+        query_embedding: list[float], 
+        collection: str, 
         ) -> list[ChromaDbResult]:
-    """
-    Queries a collection in a ChromaDB server.
-
-    Parameters:
-    query_embedding -> Embedding used to query ChromaDB, get it by calling embed() with the query str, list[float]
-    collection -> name of the collection to be queried, str
-    config -> the Configuration object with all the configs
-
-    Returns:
-    list of ChromaDbResult objects
-    """
-
-    chroma_client = chromadb.HttpClient(
-        host=config.chromadb_host, 
-        port=config.chromadb_port
+        chroma_client = chromadb.HttpClient(
+        host=self.configs.chromadb_host, 
+        port=self.configs.chromadb_port
         )
-    col = chroma_client.get_collection(name=collection)
-    raw = col.query(query_embeddings=[query_embedding], n_results=config.chroma_top_n)
-    return format_query_result(raw)
+        col = chroma_client.get_collection(name=collection)
+        raw = col.query(query_embeddings=[query_embedding], n_results=self.configs.chroma_top_n)
+        return self._format_query_result(raw)
+
+    def _format_query_result(self, raw: dict) -> list[ChromaDbResult]:
+        results = [ChromaDbResult(
+            id=raw["ids"][0][i],
+            document=raw["documents"][0][i], 
+            metadata=raw["metadatas"][0][i],
+            distance=raw["distances"][0][i]
+            ) for i in range(len(raw))]
+        return results
+
+    def _filter_results(
+            self,
+            results: list[ChromaDbResult], 
+            reranked: list[dict],
+            ) -> list[ChromaDbResult]:
+        filtered_results = []
+        print(f"reranked type: {type(reranked)}")
+        for item in reranked:
+            for result in results:
+                if result.id == item["id"]:
+                    filtered_results.append(result)
+        return filtered_results
     
 
-def format_query_result(raw: dict) -> list[ChromaDbResult]:
-    """
-    Takes the raw results from a ChromaDB query and turns them into a list of ChromaDdResult objects.
-
-    Parameters:
-    raw -> The output of query_collection(), a dictionary from a ChromaDB query
-
-    Returns:
-    A list of ChromaDbResult objects
-    """
     
-    results = [ChromaDbResult(
-        id=raw["ids"][0][i],
-        document=raw["documents"][0][i], 
-        metadata=raw["metadatas"][0][i],
-        distance=raw["distances"][0][i]
-        ) for i in range(len(raw))]
-    return results
+
     
-def filter_results(
-        results: list[ChromaDbResult], 
-        reranked: list[dict],
-        ) -> list[ChromaDbResult]:
-    filtered_results = []
-    print(f"reranked type: {type(reranked)}")
-    for item in reranked:
-        for result in results:
-            if result.id == item["id"]:
-                filtered_results.append(result)
-    return filtered_results
-    
-def handle_tool_call(
-        response_message: Message,
-        config: Configurations,
-        ) -> Message:
-    """
-    should return a Message object with "role" set to "tool"
-    """
-    tool_names = [item["name"] for item in TOOLS]
-    logger = config.logger
+# def handle_tool_call(
+#         response_message: Message,
+#         config: Configurations,
+#         ) -> Message:
+#     """
+#     should return a Message object with "role" set to "tool"
+#     """
+#     tool_names = [item["name"] for item in TOOLS]
+#     logger = config.logger
 
-    for tool_call in response_message.tool_calls:
+#     for tool_call in response_message.tool_calls:
 
-        if tool_call.function.name not in tool_names:
-            logger.error(f"The model tried to call tool {tool_call.function.name}, which is not in the list of tool names: {tool_names} ")
-            return Message(role='tool', tool_call_id=tool_call.id, content='There is no tool with that name')
-        elif tool_call.function.name == "gdpr_query":
-            print("pretty-printing the whole tool call... ")
-            pprint(tool_call)
-            arguments = json.loads(tool_call.function.arguments)
-            print(f"arguments type is: {type(arguments)}")
-            print(arguments)
+#         if tool_call.function.name not in tool_names:
+#             logger.error(f"The model tried to call tool {tool_call.function.name}, which is not in the list of tool names: {tool_names} ")
+#             return Message(role='tool', tool_call_id=tool_call.id, content='There is no tool with that name')
+        
+#         # need an elif block for each name in tool_names
+#         elif tool_call.function.name == "gdpr_query":
+#             arguments = json.loads(tool_call.function.arguments)
+#             logger.info(f"Call for tool {tool_call.function.name}: {arguments}")
+#             # gets the vector representation of the query
+#             emb_client = EmbeddingsClient(configs=config)
+#             vec_query = emb_client.embed(text=arguments["query_text"])
 
-            # gets the vector representation of the query
-            emb_client = EmbeddingsClient(configs=config)
-            vec_query = emb_client.embed(text=arguments["query_text"])
-
-            # returns list of ChromaDbResult objects
-            chromadb_response = query_collection(
-                query_embedding=vec_query, 
-                collection="gdpr", 
-                config=config
-                )
+#             # returns list of ChromaDbResult objects
+#             chromadb_response = query_collection(
+#                 query_embedding=vec_query, 
+#                 collection="gdpr", 
+#                 config=config
+#                 )
             
-            # runs the reranking
-            rerank_result = emb_client.rerank(
-                query_text=arguments["query_text"], 
-                results=chromadb_response,
-                )
+#             # runs the reranking
+#             rerank_result = emb_client.rerank(
+#                 query_text=arguments["query_text"], 
+#                 results=chromadb_response,
+#                 )
             
-            # pprint(rerank_result)
-            #applies the reranking findings
-            final_results = filter_results(results=chromadb_response, reranked=rerank_result["results"])
-            json_str = json.dumps([obj.model_dump() for obj in final_results])
-            return Message(role='tool', tool_call_id=tool_call.id, content=json_str)
+#             #applies the reranking findings
+            
             
             
 
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import requests, os
-from typing import Literal
-from pydantic import BaseModel
+import os
 from dotenv import load_dotenv
 from models import *
 from logging_setup import get_logger
-from litellm import completion
 from tools import TOOLS
-from functions import handle_tool_call, Chat, LiteLlmClient
-from dataclasses import dataclass
-from logging import Logger
+from functions import Chat, LiteLlmClient, ToolHandler
 
 if __name__ == "__main__":
     
@@ -18,7 +13,7 @@ if __name__ == "__main__":
     # get .env variables
     load_dotenv()
 
-    # get configs
+    # set configs
     configs = Configurations(
         model=os.getenv("MODEL"),
         ollama_api_base=os.getenv("OLLAMA_URL"),
@@ -31,28 +26,35 @@ if __name__ == "__main__":
         rerank_top_n=int(os.getenv("RERANK_TOP_N"))
     )
     
+    # instantiate the chat, tools, and clients
     chat = Chat(configs=configs)
     tools = [Tool(type="function", function=FunctionDefinition.model_validate(tool)) for tool in TOOLS]
-    lite_client = LiteLlmClient(configs=configs, tools=tools)
+    llm_client = LiteLlmClient(configs=configs, tools=tools)
+    tool_client = ToolHandler(configs=configs, tools=tools)
 
     while True:
         prompt_message = Message(role="user", content=input("Give me a prompt--> "))
         chat.add_message(prompt_message)
-        response = lite_client.request(messages=chat.messages)
-        response_message = lite_client.get_messsage(response=response)
+        response = llm_client.send_request(messages=chat.messages)
+        response_message = llm_client.get_messsage(response=response)
 
         # Check if the model called a tool:
         if response_message.tool_calls:
-            # tool_response should be a Message object with "role" set to "tool"
-            tool_message = handle_tool_call(response_message, configs)
+            # call the tool and get the result as a Message...
+            tool_message = tool_client.handle(response_message)
+            # ... add the message to the chat
             chat.add_message(tool_message)
-            tool_response = lite_client.request(messages=chat.messages)
-            final_response_message = lite_client.get_messsage(response=tool_response)
-            print("\nAssistant:", final_response_message.content, "\n")
+            # ... and resend the chat to the LLM:
+            tool_response = llm_client.send_request(messages=chat.messages)
+
+            # then pull the message from the response, add it to the chat, and deliver it to the user.
+            final_response_message = llm_client.get_messsage(response=tool_response)
             chat.add_message(final_response_message)
+            logger.info(f"\nAssistant: {final_response_message.content}\n")
         else:
-            print("\nAssistant:", response_message.content, "\n")
             chat.add_message(response_message)
+            logger.info(f"\nAssistant: {response_message.content}\n")
+            
 
 
 
