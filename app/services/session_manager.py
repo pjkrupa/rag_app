d50d83273763500f8a146876d324e0408c874bb6
Peter Krupa <pjkrupa@proton.me>
2025-11-29 17:10:58 +0100

    small fixes to SessionManager

--- app/services/session_manager.py
+++ app/services/session_manager.py
@@ -1,4 +1,3 @@
-import
 from core.config import Configurations
 from services.db_manager import DatabaseManager
 from services.chat import Chat
@@ -8,11 +7,9 @@ class SessionManager:
     def __init__(self, configs: Configurations, user_id: str, chat_id: int = None):
         self.configs = configs
         self.user_id = user_id
-        self.chat = self._get_chat(chat_id)
+        self.chat = Chat(user_id=user_id, configs=configs, chat_id=chat_id)
         self.db = DatabaseManager(configs=self.configs)
-    
-    def _get_chat(self, chat_id):
-        return Chat(user_id=self.user_id, configs=self.configs, chat_id=chat_id)
+
 
         
         
