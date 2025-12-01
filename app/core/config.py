c377baa1cb7ff104150d80e81f1ffac44af96181
peter <pjkrupa@proton.me>
2025-12-01 18:27:11 +0100

    cleans up main entrypoint in CLI

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from logging import Logger
+import os
 
 @dataclass
 class Configurations:
@@ -13,3 +14,18 @@ class Configurations:
     chroma_top_n: int
     rerank_top_n: int
     sqlite_path: str
+
+def set_configs(self, logger: Logger):
+    configs = Configurations(
+        model=os.getenv("MODEL"),
+        ollama_api_base=os.getenv("OLLAMA_URL"),
+        system_prompt=os.getenv("SYSTEM_PROMPT"),
+        logger=logger,
+        chromadb_host=os.getenv("CHROMADB_HOST"),
+        chromadb_port=int(os.getenv("CHROMADB_PORT")),
+        embeddings_url=os.getenv("EMBEDDINGS_URL"),
+        chroma_top_n=int(os.getenv("CHROMA_TOP_N")),
+        rerank_top_n=int(os.getenv("RERANK_TOP_N")),
+        sqlite_path=os.getenv("SQLITE_PATH")
+    )
+    return configs
--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from app.models import *
-from app.core.config import Configurations
+from app.core.config import set_configs
 from app.core.logging_setup import get_logger
 from app.tools.registry import TOOLS
 from app.services.chat import Chat
@@ -11,25 +11,8 @@ from app.services.orchestrator import Orchestrator
 
 if __name__ == "__main__":
     
-    # set up logging object
     logger = get_logger()
-
-    # get .env variables
     load_dotenv()
-
-    # set configs
-    configs = Configurations(
-        model=os.getenv("MODEL"),
-        ollama_api_base=os.getenv("OLLAMA_URL"),
-        system_prompt=os.getenv("SYSTEM_PROMPT"),
-        logger=logger,
-        chromadb_host=os.getenv("CHROMADB_HOST"),
-        chromadb_port=int(os.getenv("CHROMADB_PORT")),
-        embeddings_url=os.getenv("EMBEDDINGS_URL"),
-        chroma_top_n=int(os.getenv("CHROMA_TOP_N")),
-        rerank_top_n=int(os.getenv("RERANK_TOP_N")),
-        sqlite_path=os.getenv("SQLITE_PATH")
-    )
     
     # start a session
     print("-" * 50)
@@ -38,7 +21,7 @@ if __name__ == "__main__":
     print("-" * 50)
     print("-" * 50)
     
-    orchestrator = Orchestrator(configs=configs)
+    orchestrator = Orchestrator(configs=set_configs(logger))
     while True:
         print("Please choose: (1) Existing user; (2) Create user")
         selection = input(">> ")
@@ -62,6 +45,6 @@ if __name__ == "__main__":
     print("-" * 50)
     print("Ready for prompt.\n")
     while True:
-        print(f"Available tools: {orchestrator.tool_client.tool_names}. Attach to end of prompt with --tool_name to call.")
+        print(f"\nAvailable tools: {orchestrator.tool_client.tool_names}. Attach to end of prompt with --tool_name to call.")
         response = orchestrator.process_prompt(prompt=input("\n>> "))
         logger.info(f"Assistant: {response}")
